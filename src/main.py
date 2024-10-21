7263281cab0892a472b1a2c268637369f3a82b52
martin <martlada037@gmail.com>
2024-10-21 21:07:34 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,31 +1,31 @@
 # Initialize the structure of the graph with their connecting nodes here
-graph = {
-    'A': ['B', 'T'],
-    'B': ['A', 'C'],
-    'C': ['B', 'D'],
-    'D': ['C', 'U'],
-    'E': ['O', 'U'],
-    'F': ['U'],
-    'G': ['U'],
-    'H': ['U'],
-    'I': ['U'],
-    'J1': ['U', 'N'],
-    'J2': ['U'],
-    'K': ['U'],
-    'L': ['U'],
-    'M': ['S'],
-    'N': ['J1','S'],
-    'O': ['E', 'S'],
-    'P': ['S'],
-    'Q': ['S'],
-    'R': ['S'],
-    'S': ['M', 'N', 'O', 'P', 'Q', 'R', 'T'],
-    'T': ['A', 'S'],
-    'U': ['D', 'E', 'F', 'G', 'H', 'I', 'J1', 'J2', 'K', 'L']
+graphwcost = {
+    'A': {'B': 1, 'T': 5},
+    'B': {'A': 1, 'C': 2},
+    'C': {'B': 2, 'D': 7},
+    'D': {'C': 7, 'U': 12},
+    'E': {'O': 3, 'U': 11},
+    'F': {'U': 10},
+    'G': {'U': 8},
+    'H': {'U': 9},
+    'I': {'U': 7},
+    'J1': {'U': 5, 'N': 4},
+    'J2': {'U': 4},
+    'K': {'U': 3},
+    'L': {'U': 6},
+    'M': {'S': 11},
+    'N': {'J1': 4, 'S': 9},
+    'O': {'E': 3, 'S': 4},
+    'P': {'S': 2},
+    'Q': {'S': 3},
+    'R': {'S': 5},
+    'S': {'M': 11, 'N': 9, 'O': 4, 'P': 2, 'Q': 3, 'R': 5, 'T': 7},
+    'T': {'A': 5, 'S': 7, 'R': 5, 'Q': 3},
+    'U': {'D': 12, 'E': 11, 'F': 10, 'G': 8, 'H': 9, 'I': 7, 'J1': 5, 'J2': 4, 'K': 3, 'L': 6}
 }
 
 
-def dfs(graph, start, goal, closed_nodes):
+def dfs(graphwcost, start, goal, closed_nodes):
 
     stack = [(start, [start])]  # Format = (current node, path to current node)
     visited = []  # To keep track of the already visited nodes
@@ -45,7 +45,7 @@ def dfs(graph, start, goal, closed_nodes):
                 return path
 
             # Sort neighbors alphabetically and add unvisited neighbors to the stack
-            for neighbor in sorted(graph[node], reverse=True):
+            for neighbor in sorted(graphwcost[node], reverse=True):
                 if neighbor not in visited:
                     stack.append((neighbor, path + [neighbor]))
     # No path found
@@ -63,7 +63,7 @@ def main():
     print(f"Closed Nodes: {closed_nodes}")
 
     # Simulate traversal
-    path = dfs(graph, start, goal, closed_nodes)
+    path = dfs(graphwcost, start, goal, closed_nodes)
 
     if path:
         print(f"Path from {start} to {goal}: {' -> '.join(path)}")
